1b9afe3f53f3cad8f2689e8a5509a4532cc6c32e
milek agrawal <milekag01@gmail.com>
2020-04-21 12:46:50 +0530

    user msg and admin msg handler

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {addUser,removeUser, getUser, getUsersInRoom} = require('./users');
+
 const app = express();
 const router = require('./router');
 app.use(router);
@@ -12,8 +14,44 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('Connection Established');
 
-    socket.on('join', ({name, room}) => {
-        console.log(name, room);
+    socket.on('join', ({name, room}, callback) => {
+        // console.log(name, room);
+
+        // adding a user to room when he joins
+        const {error, user} = addUser({id: socket.id, name, room});
+        if(error) {                 // if some error occurs, callback from frontend is called with error
+            return callback(error);
+        }
+
+        // emit is used when we want to emit a event from backend to frontend
+        // .on() is used when we expect a event on backend
+
+        // Invite message to user when he joins: received by user who joins
+        socket.emit('messsage', {
+            user: 'admin',
+            text: `Hello ${user.name}! Welcome to the room ${user.room}`
+        });
+
+        // sent to all users other than the user who joins
+        socket.broadcast.to(user.room).emit('message', {
+            user: 'admin',
+            text: `${user.name} has joined !`
+        });
+
+        socket.join(user.room);
+        callback();     // calling the frontend callback when no error
+        
+    });
+
+    // user sent msgs to the room
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', {
+            user: user.name, 
+            text: message
+        });
+
+        callback(); // calling the frontend callback when no error
     })
 
     socket.on('disconnect', () => {
